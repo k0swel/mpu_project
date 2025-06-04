7d5338ac07097411ea2075c3a4a32395fa46c01a
k0swel <romanleonenko32@gmail.com>
2025-06-04 23:48:33 +0300

    Вынос методов решения уравнений из класса " functions_for_server\

--- server/include/functions_for_server.h
+++ server/include/functions_for_server.h
@@ -13,15 +13,10 @@ private:
    functions_for_server(); // прячем конструктор
    functions_for_server(const functions_for_server&); // прячем конструктор копирования.
    static functions_for_server* p_instance; // переменная для хранения единственного экземпляра класса
-   bool isZero(double number);
-
 public:
    static functions_for_server* get_instance();
    QString get_server_time(); // метод получения времени на сервере.
    bool send_email_to_client(QString email, QString code); // функция отправки кода на почту
-   QVector<QPair<double, double>> diaposons(double a, double b, double step);
-   QVector<double> find_x(const QVector<QPair<double, double>>& diapozon, double a, double b, double c);
-   double Calc(double a, double b, double c, double x); // функция, возвращающая y-значение уравнения
 
 
 
--- server/src/functions_for_server.cpp
+++ server/src/functions_for_server.cpp
@@ -1,15 +1,11 @@
 #include "functions_for_server.h"
 #include <QDebug>
 #include "SmtpMime" // импортируем библиотеку отправки сообщений на почту
+#include "math_class.h"
 
 functions_for_server* functions_for_server::p_instance = nullptr;
 
 functions_for_server::functions_for_server(){}
-
-bool functions_for_server::isZero(double number)
-{
-   return (number >=0 and number < 0.0001);
-}
 
 bool functions_for_server::send_email_to_client(QString email, QString code)
 {
@@ -63,81 +59,6 @@ QString functions_for_server::get_server_time() {
    return QString(time_format);
 }
 
-//#define EPS 0.00001
-
-QVector<QPair<double, double>> functions_for_server::diaposons(double a, double b, double step) {
-    QVector<QPair<double, double>> diaposon;
-    double a_start = a;
-    double b_start = a + step;
-    while (a_start + step <= b) {
-        diaposon.push_back({ a_start, b_start });
-        a_start = b_start;
-        b_start += step;
-    }
-    return diaposon;
-}
-
-QVector<double> functions_for_server::find_x(const QVector<QPair<double, double>>& diapozon,
-                                           double a, double b, double c) {
-    QVector<double> answers;
-    const double EPS = 1e-10;
-
-    for (auto interval : diapozon) {
-        double x1 = interval.first;
-        double x2 = interval.second;
-        double f1 = Calc(a, b, c, x1);
-        double f2 = Calc(a, b, c, x2);
-
-        // Проверка корня на левой границе
-        if (isZero(f1)) {
-            if (answers.empty() || !qFuzzyCompare(answers.last(), x1)) {
-                answers.push_back(x1);
-            }
-            continue;
-        }
-
-        // Проверка корня на правой границе
-        if (isZero(f2)) {
-            if (answers.empty() || !qFuzzyCompare(answers.last(), x2)) {
-               answers.push_back(x2);
-            }
-            continue;
-        }
-
-
-        // Поиск корня внутри интервала
-        if (f1 * f2 < 0) {
-            while (qAbs(x2 - x1) > EPS) {
-                double mid = (x1 + x2) / 2;
-                double f_mid = Calc(a, b, c, mid);
-
-                if (isZero(f_mid)) {
-                    break;
-                }
-
-                if (f1 * f_mid < 0) {
-                    x2 = mid;
-                    f2 = f_mid;
-                } else {
-                    x1 = mid;
-                    f1 = f_mid;
-                }
-            }
-
-            double root = (x1 + x2) / 2;
-            if (answers.empty() || !qFuzzyCompare(answers.last(), root)) {
-                answers.push_back(root);
-            }
-        }
-    }
-
-    return answers;
-}
-
-double functions_for_server::Calc(double a, double b, double c, double x){
-    return x*x*a + x*b + c;
-}
-
 void functions_for_server::slot_linear_equation(QString a, QString b, client* client)
 {
     bool ok1, ok2;
@@ -161,8 +82,8 @@ void functions_for_server::slot_linear_equation(QString a, QString b, client* cl
         }
     } else {
         // Определяем диапазон и шаг
-        QVector<QPair<double, double>> ans = diaposons(-1000, 1000, 1);
-        QVector<double> korni = find_x(ans, 0, coeff_a, coeff_b);
+        QVector<QPair<double, double>> ans = math_class::diaposons(-1000, 1000, 1);
+        QVector<double> korni = math_class::find_x(ans, 0, coeff_a, coeff_b);
         //Выводим корни
         solution = QString("answer|");
         for (const auto &el : korni) {
@@ -204,8 +125,8 @@ void functions_for_server::slot_quadratic_equation(QString a, QString b, QString
         emit this->signal_equation_solution("answer|no_solution");
         return;
     }
-    QVector<QPair<double, double>> ans = diaposons(-1000, 1000, 0.1);
-    QVector<double> korni = find_x(ans, coeff_a, coeff_b, coeff_c);
+    QVector<QPair<double, double>> ans = math_class::diaposons(-1000, 1000, 0.1);
+    QVector<double> korni = math_class::find_x(ans, coeff_a, coeff_b, coeff_c);
     //Выводим корни
     solution = QString("answer|");
     if (korni.size() > 0) {
